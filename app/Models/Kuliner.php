73742e210f0a00bc2ad1760871be07d2162e309d
nensi21 <nensicantik21@gmail.com>
2022-08-14 02:32:15 +0800

    g

--- app/Models/Kuliner.php
+++ app/Models/Kuliner.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-<<<<<<< HEAD
 class Kuliner extends Model
-=======
-class Berita extends Model
->>>>>>> 6f9107990c83b11e86fb21c08e2fa788fb3c2642
 {
     use HasFactory;
 
